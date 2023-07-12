ff44d063b71489c4184b514b3b0f10d068e184ca
phiggin1 <phiggin1@umbc.edu>
2023-07-12 12:22:51 -0400

    fixed service import

--- src/main.py
+++ src/main.py
@@ -2,28 +2,31 @@
 
 import rospy
 from geometry_msgs.msg import PointStamped
-
+from toy_assembly.srv import Indicate
 
 rospy.init_node('toy_assembly_main')
 
-#get images of all the possible bodies (SAM if positions known ahead of time can project points into image and get segmented images that way) 
-#get locations of all possible bodies -> can this be preknown?
-
-#dictionary of "id":{"image":"np array", "position":"PointStamped  of grasp target"}
-
-#get the users description of the body they want
-#get users gaze during description
+#get images of all the possible bodies
+    #point cloud detect bodies (zone)
+        #get locations of all possible bodies
+    #focus on each body
+    #SAM use center point of pointclout in image space as 'label'
+        #get mask -> get rgb image -> get clip embdding
+    #create list of dictionary {"image":"np array", "position":"PointStamped  of grasp target"}
 
 #while not correct_selection
-    #robot makes selection
-        #language + gaze
+    #get the users description of the body they want
+    #get users gaze during description (have long buffer of head pose or selected target with timestamps)
+    #robot makes selection based on language + gaze
     #ask if correct
-        #get audio (yes/no)
-        #get sentiment
-    #if not remove selection 
+        #if not remove selection 
+    #else
+        #correct_selection
 
+#manage the insertion process
 
 serv = rospy.ServiceProxy('indicate', Indicate)
+rospy.wait_for_service('indicate')
 
 
 #points (x,y,z)
@@ -34,9 +37,11 @@ points = [
 ]
 
 for p in points:
-    print(p)
-    point = PointStamped()
-
+    point = PointStamped()  #http://docs.ros.org/en/noetic/api/geometry_msgs/html/msg/PointStamped.html
+    point.point.x = p[0]
+    point.point.y = p[1]
+    point.point.z = p[2]
+    
+    print(point)
     resp = serv(point)
-
-
+    print(resp)
