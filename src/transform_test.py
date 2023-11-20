5226dd7815b8b71b051d2e3476046bc538ba0dda
phiggin1 <phiggin1@umbc.edu>
2023-11-20 13:29:19 -0500

    Update servo config

--- src/transform_test.py
+++ src/transform_test.py
@@ -8,9 +8,9 @@ import numpy as np
 from std_msgs.msg import String
 from geometry_msgs.msg import PointStamped, Point
 
-CAMERA = "/gen3_robotiq_2f_85_left/world/base_link/shoulder_link/bicep_link/forearm_link/spherical_wrist_1_link/spherical_wrist_2_link/bracelet_link/end_effector_link/camera_link/camera_standin"
-#CAMERA = "/Player/NoSteamVRFallbackObjects/FallbackObjects/camera"
-OBJECT = "/horse_body (2)"
+#CAMERA = "/gen3_robotiq_2f_85_left/world/base_link/shoulder_link/bicep_link/forearm_link/spherical_wrist_1_link/spherical_wrist_2_link/bracelet_link/end_effector_link/camera_link/camera_standin"
+CAMERA = "/Player/NoSteamVRFallbackObjects/FallbackObjects/camera"
+OBJECT = "/horse_body"
 
 def Unity2Ros(vector3):
     #return (vector3.z, -vector3.x, vector3.y);
@@ -24,6 +24,7 @@ class TestTracker:
             self.target_name = rospy.get_param("target_name", OBJECT)
             self.scene_transform_topic = rospy.get_param("transform_topic", "/scene/transform")
             self.target_point_topic = rospy.get_param("target_topic", "/pt")
+            self.target_pose_topic = rospy.get_param("target_pose_topic", "/pose")
 
             self.sub = rospy.Subscriber(self.scene_transform_topic, String, self.transform_cb)
             self.pub = rospy.Publisher(self.target_point_topic, PointStamped, queue_size=10)
@@ -48,18 +49,37 @@ class TestTracker:
             m = tf.transformations.compose_matrix(None, None, (roll, pitch, yaw), (p_x,p_y,p_z), None)
             transform_to_world[name]=m
 
-            
-            print(name)
-            print(p_x,p_y,p_z)
-            #print(roll,pitch,yaw)
-            #print(m)
-            print('-----')
+            rospy.loginfo(f"{name}")
+            rospy.loginfo(f"{p_x},{p_y},{p_z}")
+            #rospy.loginfo(roll,pitch,yaw)
+            #rospy.loginfo(f"\n{m}")
+            rospy.loginfo('-----')
             
             
         object_to_camera = np.matmul(transform_to_world[self.target_name], np.linalg.inv(transform_to_world[self.camera_name]))
         
         point = np.zeros( (4,1) )
         point[3,0]=1
+
+
+        '''
+        # mat44 is frame-to-frame transform as a 4x4
+        mat44 = object_to_camera
+
+        # pose44 is the given pose as a 4x4
+        p = Point()
+        q = Quat()
+        position = tf.transformations.xyz_to_mat44(ps.pose.position)
+        orientation = tf.transformations.xyzw_to_mat44(ps.pose.orientation)
+        pose44 = numpy.dot(position, orientation)
+
+        # txpose is the new pose in target_frame as a 4x4
+        txpose = numpy.dot(mat44, pose44)
+
+        # xyz and quat are txpose's position and orientation
+        xyz = tuple(transformations.translation_from_matrix(txpose))[:3]
+        quat = tuple(transformations.quaternion_from_matrix(txpose))
+        '''
 
         object_position_in_camera = np.matmul(object_to_camera, point)
 
@@ -73,8 +93,8 @@ class TestTracker:
         p.point.y = y
         p.point.z = z
         
-        print(p.point)
-        print("=====")
+        rospy.loginfo(p.point)
+        rospy.loginfo("=====")
         
         self.pub.publish(p)
         
--- transcribe.py
+++ transcribe.py
@@ -1,28 +1,31 @@
 
-import zmq
+#import zmq
 import sys
 import time
 #import whisper
-
+import argparse
 
 class Transcribe:
-    def __init__(self):
-        sever_address = "iral-pinky.cs.umbc.edu"
-        sever_port  = "8888"
+    def __init__(self, hostname, port):
+        sever_address = hostname
+        sever_port  = port
 
+        print(f"Connecting to {sever_address}:{sever_port}")
 
         #self.whisper_model = whisper.load_model("base")   
 
-        context = zmq.Context()
-        self.socket = context.socket(zmq.PAIR)
-        self.socket.connect("tcp://"+sever_address+":"+sever_port)
+        #context = zmq.Context()
+        #self.socket = context.socket(zmq.PAIR)
+        #self.socket.connect("tcp://"+sever_address+":"+sever_port)
+        print(f"Connected to {sever_address}:{sever_port}")
+
         self.tmp_audio_filename = '/tmp/audio.mp3'
 
     def run(self):
         while True:
-            msg = self.socket.recv()
-            self.recv_msg(msg)
-        
+            #msg = self.socket.recv()
+            #self.recv_msg(msg)
+            print('s')
 
     def recv_msg(self, msg):
         #save to /tmp/audio.mp3
@@ -42,6 +45,15 @@ class Transcribe:
 
 
 if __name__ == '__main__':
-    t = Transcribe()
-    t.run()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--hostname", default="iral-pinky.cs.umbc.edu", required=False,
+                        help="hostname for ROS system running transcribe_server.py")
+    parser.add_argument("--port", default="8888", required=False,
+                        help="port transcribe_server.py is listening on.")
+    args = parser.parse_args()
+    hostname = args.hostname
+    port = args.port
+    
+    t = Transcribe(hostname=hostname,port=port)
+    #t.run()
 
