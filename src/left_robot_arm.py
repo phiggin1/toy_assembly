17938a5df4b27b35c0a71b735e071a2c9571cd3f
a-r-joseph <abbyj1@umbc.edu>
2024-04-04 14:33:20 -0400

    add left and right robot movement scripts

---
+++ src/left_robot_arm.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+
+import sys
+import rospy
+import numpy as np
+import cv2
+from cv_bridge import CvBridge
+from std_msgs.msg import String
+from sensor_msgs.msg import Image, CameraInfo, PointCloud2
+from image_geometry import PinholeCameraModel
+import sensor_msgs.point_cloud2 as pc2
+from toy_assembly.srv import SAM
+from toy_assembly.srv import CLIP
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Pose
+from obj_segmentation.msg import SegmentedClustersArray
+
+
+class Left_arm:
+    def __init__(self):
+        rospy.init_node("leftArm")
+
+        self.start_pose = [-0.88708, -0.01635, -2.04250, -2.17897, 1.67276, 2.33558]
+        self.goal_tolerance = 0.01
+        self.init_position()
+
+        self.cam_info = rospy.wait_for_message('/unity/camera/left/depth/camera_info', CameraInfo)
+        self.cam_model = PinholeCameraModel()
+        self.cam_model.fromCameraInfo(self.cam_info)
+
+        self.cluster_topic = "/filtered_object_clusters"
+        self.clusters = rospy.wait_for_message(self.cluster_topic, SegmentedClustersArray)
+
+        self.horses_pose = []
+
+        self.horse_pose_pub = rospy.Publisher('/object_positions', PoseStamped, queue_size=10)  # may need to edit topics
+
+        # pose array has a specific format but i can make those zeros so dont have to worry about setting up the message, hence just sending the position
+
+        while not rospy.is_shutdown():
+            self.get_horses_pose(self.clusters);
+        
+    def init_position(self):
+        moveit_commander.roscpp_initialize(sys.argv)
+        #rospy.init_node("move_to_start", anonymous=True)
+        self.robot = moveit_commander.RobotCommander()
+        self.scene = moveit_commander.PlanningSceneInterface()
+        self.arm_group_name = "arm"
+        self.arm_move_group = moveit_commander.MoveGroupCommander(self.arm_group_name)
+        self.planning_frame = self.arm_move_group.get_planning_frame()
+
+        self.arm_move_group.set_max_velocity_scaling_factor(0.750)
+        self.arm_move_group.set_goal_joint_tolerance(self.goal_tolerance)
+        self.arm_move_group.go(self.start_pose, wait=True)       
+        self.arm_move_group.stop()
+
+    def get_horses_pose(self, clusters):
+        for i, pc in enumerate(clusters.clusters):
+            #print(f"obj {i}")
+            p = self.get_centroid(pc)
+            self.horses_pose.append(p)
+
+        """
+        dont need to convert to pixel here. send locations as is under the 3d frame of reference of the left arm to the right arm, 
+        where you can use the header message to then change the frame id to that of the right arm and move from there.
+        """
+        first_obj_pos = PoseStamped()
+        first_obj_pos.header.frame_id = clusters.header.frame_id
+        first_obj_pos.pose.position.x = self.horses_pose[0][0]
+        first_obj_pos.pose.position.y = self.horses_pose[0][1]
+        first_obj_pos.pose.position.z = self.horses_pose[0][2]
+        first_obj_pos.header.stamp = rospy.Time.now()
+        self.horse_pose_pub.publish(first_obj_pos)
+
+    def get_centroid(self, pc):
+        min_x = 1000.0        
+        min_y = 1000.0
+        min_z = 1000.0
+        max_x = -1000.0
+        max_y = -1000.0
+        max_z = -1000.0
+
+        #for each object get a bounding box
+        for p in pc2.read_points(pc):
+            if p[0] > max_x:
+                max_x = p[0]
+            if p[0] < min_x:
+                min_x = p[0]
+
+            if p[1] > max_y:
+                max_y = p[1]
+            if p[1] < min_y:
+                min_y = p[1]
+
+            if p[2] > max_z:
+                max_z = p[2]
+            if p[2] < min_z:
+                min_z = p[2]
+
+        center = [(min_x + max_x)/2, (min_y + max_y)/2, (min_z + max_z)/2]
+        w = max_x-min_x
+        h = max_y-min_y
+        d = max_z-min_z
+
+        return center
+
+
+if __name__ == '__main__':
+    left_robot = Left_arm()
---
+++ src/right_robot_arm.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+
+import sys
+import rospy
+import numpy as np
+import math
+import cv2
+from cv_bridge import CvBridge
+from std_msgs.msg import String
+from sensor_msgs.msg import Image, CameraInfo, PointCloud2
+from image_geometry import PinholeCameraModel
+import sensor_msgs.point_cloud2 as pc2
+from toy_assembly.srv import SAM
+from toy_assembly.srv import CLIP
+import moveit_commander
+import moveit_msgs.msg
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import PoseStamped 
+import tf
+from tf.transformations import quaternion_from_euler
+import json
+
+class Right_arm:
+    def __init__(self):
+        rospy.init_node("rightArm")
+
+        self.start_pose = [0.00062, -0.12636, -1.07251, -0.00121, -2.11940, 1.57205]
+        self.init_position()	
+
+        self.cam_info = rospy.wait_for_message('/unity/camera/right/rgb/camera_info', CameraInfo)
+        self.cam_model = PinholeCameraModel()
+        self.cam_model.fromCameraInfo(self.cam_info)
+        
+        self.horse_topic = '/object_positions'
+        self.horse_pose = None  
+        self.horses = rospy.Subscriber(self.horse_topic, PoseStamped, self.get_horse_pose)
+        #self.horse_pose = []
+
+        self.listener = tf.TransformListener()
+        self.grab = rospy.Publisher('/buttons', String, queue_size=10)
+
+        """
+        self.servo_pub = rospy.Publisher('/my_gen3_right/servo_server/delta_twist_cmds', TwistStamped, queue_size=10)  # may need to edit topics
+        while not rospy.is_shutdown():
+            if self.horse_pose:
+                self.get_object(self.horse_pose)
+        """ 
+        #print(self.horse_pose)
+        success = False
+        while not rospy.is_shutdown():
+            if (self.horse_pose is not None):
+                """
+                create function tranfomr_obj_pos
+                update time message on publisher to be able to transform
+                publish PosStamped instead of pointstamped
+                """
+                if (not success):
+                    #obj_pos = self.transform_obj_pos(self.horse_pose)
+                    self.get_object(self.horse_pose)
+                    success = True
+                else:
+                    break
+            #else:
+                #print('fail')
+                #break
+	
+    def init_position(self):
+        moveit_commander.roscpp_initialize(sys.argv)
+        #rospy.init_node("move_to_start", anonymous=True)
+        self.robot = moveit_commander.RobotCommander()
+        self.scene = moveit_commander.PlanningSceneInterface()
+        self.arm_group_name = "arm"
+        self.arm_move_group = moveit_commander.MoveGroupCommander(self.arm_group_name)
+        self.planning_frame = "right_base_link"
+
+        self.arm_move_group.set_max_velocity_scaling_factor(0.750)
+        #self.arm_move_group.set_goal_position_tolerance(self.goal_tolerance)
+        self.arm_move_group.go(self.start_pose, wait=True) 
+        self.arm_move_group.stop()
+
+    def get_horse_pose(self, pose):
+        """
+        gets the 3d location of objects in space specified in the cluster
+        """
+        #del self.horse_pose[:]
+        print(f'pose: {pose}')
+        self.horse_pose = self.transform_obj_pos(pose)
+        print(f'transformed: {self.horse_pose}')
+
+    def get_object(self, object_pose):
+        #print(object_pose)
+        pose_goal = Pose()
+        #print('timeout1')
+        pose_goal.position = object_pose.pose.position
+        quat = quaternion_from_euler(math.pi, 0.0, 0.0)
+        #print(quat)
+        pose_goal.orientation.x = quat[0]
+        pose_goal.orientation.y = quat[1]
+        pose_goal.orientation.z += 0.1
+        pose_goal.orientation.w = quat[3]
+        #print(pose_goal.orientation)
+
+        #print('timeout 2')
+        self.arm_move_group.set_pose_target(pose_goal)
+
+        #print('timeout3')
+        print(pose_goal)
+        self.arm_move_group.go(pose_goal, wait=True)
+        self.arm_move_group.stop()
+        self.arm_move_group.clear_pose_targets()
+
+        #print(object_pose)
+        pose_goal2 = Pose()
+        #print('timeout1')
+        pose_goal2.position = object_pose.pose.position
+        quat = quaternion_from_euler(math.pi, 0.0, 0.0)
+        #print(quat)
+        pose_goal2.orientation.x = quat[0]
+        pose_goal2.orientation.y = quat[1]
+        pose_goal2.orientation.z = quat[2]
+        pose_goal2.orientation.w = quat[3]
+        #print(pose_goal2.orientation)
+        
+        self.arm_move_group.go(pose_goal2, wait = True)
+        self.arm_move_group.stop()
+        self.arm_move_group.clear_pose_targets()
+ 
+        """
+        publishing "grabbed" vs. "released" will do as follows in unity with the nearest object
+        """
+        a = dict()
+        a["robot"] = "right"
+        a["action"] = "grab"
+        s = json.dumps(a)
+        self.grab.publish(s)
+        
+        
+    def transform_obj_pos(self, obj_pos):
+        t = rospy.Time.now()
+        obj_pos.header.stamp = t
+        self.listener.waitForTransform(obj_pos.header.frame_id, self.planning_frame, t, rospy.Duration(4.0))
+        obj_pos = self.listener.transformPose(self.planning_frame, obj_pos)
+        return obj_pos
+	
+        
+if __name__ == '__main__':
+    right_robot = Right_arm()
