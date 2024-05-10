fdbb1503e49174385465740cc582b04ef6087531
phiggin1 <phiggin1@umbc.edu>
2024-05-10 13:55:08 -0400

    python float is not  np.float32

--- src/audio_recorder.py
+++ src/audio_recorder.py
@@ -5,29 +5,41 @@ import pyaudio
 import numpy as np
 from std_msgs.msg import Float32MultiArray
 
-CHANNELS = 1 
-CHUNK = 1024
-FORMAT = pyaudio.paFloat32
-RATE = 16000
 
 rospy.init_node("audio_recorder")
 
-pub = rospy.Publisher("/audio", Float32MultiArray)
+publish_rate = 10
+ros_pub_rate = rospy.Rate(publish_rate)
+
+CHANNELS = 1 
+FORMAT = pyaudio.paFloat32
+RATE = 16000
+
+print(RATE/publish_rate)
+print(FORMAT)
+
+CHUNK = 1024
+
+pub = rospy.Publisher("/audio", Float32MultiArray, queue_size=10)
 
 pa = pyaudio.PyAudio()
-stream = pa.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True)
+stream = pa.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
 
 while not rospy.is_shutdown():
     #read in audio clip
     audio = stream.read(CHUNK)
 
     #get the float array
-    float_array = np.fromstring(audio, dtype=float)
+    float_array = np.fromstring(audio, dtype=np.float32)
+    
+    print(float_array.shape)
+
     float_array_msg = Float32MultiArray()
     float_array_msg.data = float_array
 
     #publish
     pub.publish(float_array_msg)
+    #ros_pub_rate.sleep()
 
 stream.close()
 pa.terminate()
